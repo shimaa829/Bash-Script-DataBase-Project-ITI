ed93eaf615d2111a5c3c2bc1e8dc1317b994bb1c
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-01-24 14:23:58 +0200

    modifictions on create_table script

--- scripts/delete_DataBase.sh
+++ scripts/delete_DataBase.sh
@@ -2,50 +2,64 @@
 
 path_of_dataBase=~/dataBase_Engin
 
-echo "Enter the name of dataBase ?"
-read name
-
-
 
 #Check if the DataBase Engin is initialized
 
 if [ ! -d ~/dataBase_Engin ]
 then
    
-   echo "Your DataBase Engin isn't initialized ,, please choose the first option:"
+    echo "Your DataBase Engin isn't initialized ,, please choose the first option:"
     source ~/Bash-Script-DataBase-Project-ITI/dataBase_Options.sh
 
 else
 
-  
+    cd ~/dataBase_Engin
 
-   # cd "$path_of_dataBase"
-   pwd
-  
-    #check if the dataBase_name Directory exist
-    if [ -d "$name" ]
+    arr_dataBases=($(ls))
+
+    #check if there are dataBases exist
+    if [[ ${#arr_dataBases[@]} > 0 ]]
     then
-    
-        rm -r "$name"
+         
+        #Display all DataBases
+        ls
 
-        echo "your DataBase is deleted"
+        echo "Enter the dataBase_name of dataBase ?"
+        read dataBase_name
+
+        #check if the dataBase_dataBase_name Directory exist
+        if [ -d "$dataBase_name" ]
+        then
+        
+            rm -r "$dataBase_name"
+
+            echo "your DataBase is deleted"
+
+        else
+            
+            echo "this dataBase isn't exist"
+
+            echo  "Go back to menu? yes / no" 
+
+            read answer
+
+            if [ $answer = "yes" ]
+            then
+
+                source ~/Bash-Script-DataBase-Project-ITI/dataBase_Options.sh
+                
+            else
+                echo "Please select dataBase option"
+                source ~/Bash-Script-DataBase-Project-ITI/dataBase_Options.sh
+
+            fi
+
+        fi
 
     else
         
-        echo "this dataBase isn't exist"
-
-        echo  "Go back to menu? yes / no" 
-
-        read answer
-
-        if [ $answer = "yes" ]
-        then
-            source ~/Bash-Script-DataBase-Project-ITI/dataBase_Options.sh
-            
-        else
-            echo "Please select dataBase option"
-
-        fi
+        echo "This DataBase Engin is empty ,, please create database first"
+        source ~/Bash-Script-DataBase-Project-ITI/dataBase_Options.sh
 
     fi
 
