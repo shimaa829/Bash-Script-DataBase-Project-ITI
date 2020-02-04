6308f2a906e6e45a90b0a9d13fbda715c88d5eb6
m-bassam94 <mohamedbassam094@gmail.com>
2020-02-04 09:42:17 +0200

    fix bugs

--- scripts/remove_record.sh
+++ scripts/remove_record.sh
@@ -16,18 +16,18 @@ read -a arr_Of_Columns  <<< `sed '9!d' $tableName`
  echo " "
 
  removeRecord()
-{
- printf "\nEnter the pk value of the record you want to remove:\n"
- read pk_value 
+    {
+    printf "\nEnter the pk value of the record you want to remove:\n"
+    read pk_value 
 
- #Search to check if the selected pk_value exists
- typeset -i pk_field_number=$pk+1
- if [[ $(sed '1,9d' $tableName | cut -d " " -f $pk_field_number | grep -x $pk_value | sed '1!d') =  $pk_value ]]
-				   then 
-                    no_of_record=$(cut -d " " -f $pk_field_number $tableName | grep -xn $pk_value | cut -d: -f 1)
-                    sed "$no_of_record"d $tableName > new_file
-                    cat new_file > $tableName
-				    rm new_file
- fi                   
-}
+    #Search to check if the selected pk_value exists
+    typeset -i pk_field_number=$pk+1
+     if [[ $(sed '1,9d' $tableName | cut -d " " -f $pk_field_number | grep -x $pk_value | sed '1!d') =  $pk_value ]]
+				        then 
+                        no_of_record=$(cut -d " " -f $pk_field_number $tableName | grep -xn $pk_value | cut -d: -f 1)
+                        sed "$no_of_record"d $tableName > new_file
+                        cat new_file > $tableName
+	    			    rm new_file
+    fi                   
+    }
 removeRecord
